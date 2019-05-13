3a154f4eb849c595fa5111253ab535c5de84f35f
wangxingdi <forwangxd@gmail.com>
2019-05-13 23:39:19 +0800

    静态文件库更换

--- admincp/header.php
+++ admincp/header.php
@@ -47,7 +47,7 @@ if($SiteSettings = $mysqli->query("SELECT * FROM settings WHERE id='1'")){
       <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
 <![endif]-->
 
-<script src="js/jquery.min.js"></script>  
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
 <script src="js/bootstrap.min.js"></script>
 <script src="js/jquery.menu.js"></script>
 <script src="js/jquery.pjax.js" type="text/javascript"></script>
