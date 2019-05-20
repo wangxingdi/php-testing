467b07f75e1e512eb8616b1240d7dd62bec4b40b
wangxingdi <forwangxd@gmail.com>
2019-05-20 23:41:16 +0800

    个性化

--- offer_link.php
+++ offer_link.php
@@ -8,8 +8,12 @@ if ($products_result_set = $mysqli->query("SELECT * FROM mp_products WHERE produ
 } else {
     printf("<div class='alert alert-danger alert-pull'>查询产品失败(offer_link.php)</div>");;
 }
-$mysqli->query("UPDATE mp_products SET product_hits=product_hits+1 WHERE product_id='$product_id'");
-header('Location: ' . $product_affiliate_url . '');
+if(empty($product_affiliate_url)){
+    echo "抱歉，您寻找的产品【" .  $products_row['product_name'] . "】在国内暂时找不到卖家，我们会尽快寻找并上架！";
+}else{
+    $mysqli->query("UPDATE mp_products SET product_hits=product_hits+1 WHERE product_id='$product_id'");
+    header('Location: ' . $product_affiliate_url . '');
+}
 ?>
 <!doctype html>
 <html>
